dc01383a949a1b31a3ee6e4730c719d6537b71a9
Joel Christopher <118204275+joelchristopher-d@users.noreply.github.com>
2024-07-17 22:45:21 +0530

    Update Subsets.py

--- Subsets.py
+++ Subsets.py
@@ -5,3 +5,15 @@ class Solution:
             print(result,num)
             result += [curr + [num] for curr in result]
         return result
+
+
+
+
+def backtrack(start, path,nums):
+    print(start,result,path)
+    result.append(path)
+    for i in range(start, len(nums)):
+        backtrack(i + 1, path + [nums[i]],nums)
+
+result = []
+backtrack(0, [],[1,2,3])
