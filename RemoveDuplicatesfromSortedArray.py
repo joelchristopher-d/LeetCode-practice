81e211a96152bc68b2c867369b91f686d49cc4ef
Joel Christopher <118204275+joelchristopher-d@users.noreply.github.com>
2024-09-11 22:30:56 +0530

    Update RemoveDuplicatesfromSortedArray.py

--- RemoveDuplicatesfromSortedArray.py
+++ RemoveDuplicatesfromSortedArray.py
@@ -11,4 +11,20 @@ class Solution:
                     nums.pop(high)
                 high-=1
         return len(nums)
+
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        low=0
+        high=low+1
+        while(high<len(nums)):
+            if nums[low]==nums[high]:
+                high+=1
+            else:
+                nums[low+1]=nums[high]
+                low+=1
+                high+=1
+        return low+1
+       
        
