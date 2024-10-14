7d702d51949a2a3cb10e62a7c1b15325afa5db06
Joel Christopher <118204275+joelchristopher-d@users.noreply.github.com>
2024-10-14 21:43:47 +0530

    Update Sqrt(x).py

--- Sqrt(x).py
+++ Sqrt(x).py
@@ -8,3 +8,19 @@ class Solution:
         while b*b > x:
             b = (b+x//b)//2
         return b
+
+
+n = 36
+def square(n):
+    low = 0
+    high = n
+    while(low<=high):
+        mid = (low+high)//2
+        print(low,high,mid)
+        if mid*mid == n:return mid
+        if mid*mid > n:
+            high = mid-1
+        else:
+            low = mid+1
+    return high
+square(10)
