ea91b6b199a1748a75a778748d576100136932de
Joel Christopher <118204275+joelchristopher-d@users.noreply.github.com>
2024-12-19 21:29:39 +0530

    Update MedianofTwoSortedArrays.py

--- MedianofTwoSortedArrays.py
+++ MedianofTwoSortedArrays.py
@@ -1,6 +1,22 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        l=sorted(nums1+nums2)
-        l1=0
-        return float(f"{l[(len(l)-0)//2]:.5f}" if len(l)%2!=0 else f"{(l[(len(l)-0)//2]+l[((len(l)-0)//2)-1])/2:.5f}")
-        
+            left = right = 0
+            res = []
+            n1 = len(nums1)
+            n2 = len(nums2)
+            while left<n1 and right<n2:
+                if nums1[left]<=nums2[right]:
+                    res.append(nums1[left])
+                    left+=1
+                else:
+                    res.append(nums2[right])
+                    right+=1
+            while left<n1:
+                res.append(nums1[left])
+                left+=1
+            while right<n2:
+                res.append(nums2[right])
+                right+=1
+                    
+            return res[len(res)//2] if len(res)%2!=0 else (res[len(res)//2]+res[(len(res)//2)-1])/2
+                
